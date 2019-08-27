489e6d86afb61589ace3a48033d3c32cf36678f0
ujjwal <ujjwalmaity98@gmail.com>
2019-08-27 21:17:51 +0530

    Edit Actionbar

--- app/src/main/java/com/ujjwal/cessini/MainActivity.java
+++ app/src/main/java/com/ujjwal/cessini/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Explore");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
     }
 }
