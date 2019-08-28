b8149165a5bca8a4bf559025d0642c2d177c6cf0
ujjwal <ujjwalmaity98@gmail.com>
2019-08-28 06:33:55 +0530

    Design miworld analysis

--- app/src/main/java/com/ujjwal/cessini/Main2Activity.java
+++ app/src/main/java/com/ujjwal/cessini/Main2Activity.java
@@ -7,7 +7,19 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
+
 public class Main2Activity extends AppCompatActivity {
+
+    BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,51 @@ public class Main2Activity extends AppCompatActivity {
         getSupportActionBar().setTitle("Your Analytic");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+
+        barChart = findViewById(R.id.chart);
+
+        setData();
+    }
+
+    private void setData() {
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(0, 1100));
+        barEntries.add(new BarEntry(1, 1300));
+        barEntries.add(new BarEntry(2, 1550));
+        barEntries.add(new BarEntry(3, 400));
+        barEntries.add(new BarEntry(4, 1100));
+        barEntries.add(new BarEntry(5, 700));
+        barEntries.add(new BarEntry(6, 1800));
+        BarDataSet barDataSet = new BarDataSet(barEntries, "User Activity");
+        barDataSet.setDrawValues(false);
+
+        BarData barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        barChart.invalidate();  // refresh
+
+        barChart.getAxisRight().setDrawGridLines(false);
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getXAxis().setDrawGridLines(false);
+
+        YAxis rightYAxis = barChart.getAxisRight();
+        rightYAxis.setEnabled(false);
+
+        barChart.setTouchEnabled(false);
+
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("Mon");
+        labels.add("Tue");
+        labels.add("Wed");
+        labels.add("Thu");
+        labels.add("Fri");
+        labels.add("Sat");
+        labels.add("Today");
+        barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(labels));
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        barChart.getDescription().setEnabled(false);
     }
 
     @Override
